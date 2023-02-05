48f502dd946b448f851615171bfb5eb56c6f6cf7
basketgood1 <123250060+basketgood1@users.noreply.github.com>
2023-02-05 08:30:57 +0000

    add java project

---
+++ ActionTest/lib/src/main/java/ActionTest/Komina.java
@@ -0,0 +1,10 @@
+package ActionTest;
+
+public class Komina {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kombini");
+	}
+
+}
---
+++ ActionTest/lib/src/main/java/ActionTest/Main.java
@@ -0,0 +1,10 @@
+package ActionTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("He110 World!");
+	}
+
+}
